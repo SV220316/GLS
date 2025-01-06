736d089cdf27577766f536704835e2536b1105e9
SinB <74192397+SV220316@users.noreply.github.com>
2025-01-06 15:46:53 +0900

    Update Core.js

--- Core.js
+++ Core.js
@@ -14,3 +14,19 @@ async function start_front_Camera() {
       console.error("Software Error", error);
     }
   }
+function enterFullscreen() {
+    const videoElement = document.documentElement; // The root HTML element
+
+    if (videoElement.requestFullscreen) {
+      videoElement.requestFullscreen();
+    } else if (videoElement.mozRequestFullScreen) { // Firefox
+      videoElement.mozRequestFullScreen();
+    } else if (videoElement.webkitRequestFullscreen) { // Chrome, Safari, Opera
+      videoElement.webkitRequestFullscreen();
+    } else if (videoElement.msRequestFullscreen) { // IE/Edge
+      videoElement.msRequestFullscreen();
+    }
+  }
+
+  // Event listener for fullscreen button click
+  document.getElementById('fullscreen-btn').addEventListener('click', enterFullscreen);
